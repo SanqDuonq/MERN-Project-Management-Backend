372763b38db036098ac4a8a753f28aa49d8caa6e
SanqDuonq <namsang0902s@gmail.com>
2025-04-11 02:12:08 +0700

    feat: error handler

---
+++ middlewares/error-handler.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express"
+import { HttpError } from "http-errors";
+
+const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+    console.log('Error', error);
+    if (error instanceof HttpError) {
+        res.status(error.status ).json({
+            message: error.message
+        })
+        return;
+    }
+    res.status(500).json({
+        message: 'Internal Server Error'
+    })
+    return;
+}
+
+export default errorHandler;
