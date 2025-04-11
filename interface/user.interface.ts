f8c0531732cfe15407d46da072dc454bffbe8443
SanqDuonq <namsang0902s@gmail.com>
2025-04-11 12:08:20 +0700

    feat: user model

---
+++ interface/user.interface.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+    name: string,
+    email: string,
+    password?: string,
+    profilePicture?: string,
+    isActive: boolean,
+    lastLogin: Date,
+    currentWorkspace: mongoose.Types.ObjectId
+}
